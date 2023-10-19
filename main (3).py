f57d3c0dac7c1690234198cac81ad849ea714f79
Meenadevisu <148435573+Meenadevisu@users.noreply.github.com>
2023-10-19 13:46:59 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,14 @@
+
+for index, product in enumerate(productList): Lf product targetProduct: indices.append(Index)
+
+return indices
+
+I
+
+# Example spaget
+
+products ["shoes", "boot", "Loafer", "shoes", "sandal", "shoes"]
+
+target "shoes" target2="apple
+
+result linearSearchProduct(products, target) print(result)
